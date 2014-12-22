5aa128a8a9c07ed6442c17c2836d41bd6a4936e0
Matt Morris <matt.morris@placewise.com>
2014-12-22 02:25:36 -0700

    add startup message to server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var port = 8888;
+
+console.log('listening to localhost on port...' + port);
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8888);
+connect().use(serveStatic(__dirname)).listen(port);

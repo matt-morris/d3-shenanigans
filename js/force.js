9f3bd583f0f43a378c0658ecd7587582fba9f1f1
Matt Morris <matt.morris@placewise.com>
2014-12-23 14:11:23 -0700

    rewrite graph class based on this example: http://stackoverflow.com/a/9544595/1804624

--- js/force.js
+++ js/force.js
@@ -3,48 +3,111 @@ console.log('force.js');
 d3.select('body').append('style')
   .text('.node {stroke: #fff;stroke-width: 1.5px;}.link {stroke: #999;stroke-opacity:.6;}');
 
-var width  = 960,
-    height = 800;
 
-var nodes = [],
-    links = [];
+function Graph(el) {
+    this.addNode = function (node) {
+      node = node || {};
+      node.id = node.id || nodes.length;
+      nodes.push(node);
+      update();
+    };
 
-var color = d3.scale.category20();
+    this.removeNode = function (id) {
+        var i = 0;
+        var n = findNode(id);
+        while (i < links.length) {
+          if ((links[i]['source'] === n) || (links[i]['target'] == n)) links.splice(i, 1);
+          else i++;
+        }
+        var index = findNodeIndex(id);
+        if (index !== undefined) {
+          nodes.splice(index, 1);
+          update();
+        }
+    };
 
-var svg = d3.select('body').append('svg')
-            .attr('width', width)
-            .attr('height', height);
+    this.addLink = function (sourceId, targetId) {
+        var sourceNode = findNode(sourceId);
+        var targetNode = findNode(targetId);
 
-var force = d3.layout.force()
-              .size([width, height])
-              .nodes(nodes)
-              .links(links)
-              .charge(-120)
-              .linkDistance(30)
-              .start();
+        if ((sourceNode !== undefined) && (targetNode !== undefined)) {
+            links.push({ source: sourceNode, target: targetNode});
+            update();
+        }
+    };
 
-var link = svg.selectAll('.link')
-      .data(links)
-    .enter().append('line')
-      .attr('class', 'link')
-      .style('stroke-width', function(d) { return Math.sqrt(d.value); });
+    var findNode = function (id) {
+        for (var i=0; i < nodes.length; i++) {
+            if (nodes[i].id === id)
+                return nodes[i];
+        }
+    };
 
-var node = svg.selectAll('.node')
-    .data(nodes)
-  .enter().append('circle')
-    .attr('class', 'node')
-    .attr('r', 5)
-    .style('fill', function(d) { return color(d.id); })
-    .call(force.drag);
-    
-node.append('title').text(function(d) { return d.name; });
+    var findNodeIndex = function (id) {
+        for (var i=0; i < nodes.length; i++) {
+            if (nodes[i].id === id)
+                return i;
+        }
+    };
 
-force.on('tick', function() {
-  link.attr('x1', function(d) { return d.source.x; })
-      .attr('y1', function(d) { return d.source.y; })
-      .attr('x2', function(d) { return d.target.x; })
-      .attr('y2', function(d) { return d.target.y; });
+    // set up the D3 visualisation in the specified element
+    var w = 960,
+        h = 800;
 
-  node.attr('cx', function(d) { return d.x; })
-      .attr('cy', function(d) { return d.y; });
-});
+    var color = d3.scale.category20();
+    var vis = this.vis = d3.select(el).append('svg:svg')
+        .attr('width', w)
+        .attr('height', h);
+
+    var force = d3.layout.force()
+                  .gravity(0.05)
+                  .distance(100)
+                  .charge(-100)
+                  .size([w, h]);
+
+    var nodes = force.nodes(),
+        links = force.links();
+
+    var update = function() {
+      var link = vis.selectAll('line.link')
+                    .data(links, function(d) { return d.source.id + '-' + d.target.id; });
+
+      link.enter().insert('line')
+          .attr('class', 'link');
+
+      link.exit().remove();
+
+      var node = vis.selectAll('g.node')
+                    .data(nodes, function(d) { return d.id; });
+
+      var nodeEnter = node.enter().append('g')
+                          .attr('class', 'node')
+                          .call(force.drag);
+
+      nodeEnter.append('circle')
+          .attr('class', 'node')
+          .attr('r', 5)
+          .style('fill', function(d) { return color(d.id); })
+          .call(force.drag);
+
+      node.append('title')
+          .text(function(d) { return d.id + (d.name ? ': \n' + d.name : ''); });
+
+      node.exit().remove();
+
+      force.on('tick', function() {
+        link.attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; });
+
+        node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+      });
+
+      // Restart the force layout.
+      force.start();
+  };
+
+  // Make it all go
+  update();
+}

9ac366cacabc82c39c2095b5a39fe8aa8df2b3be
Matt Morris <matt.morris@placewise.com>
2014-12-24 22:43:21 -0700

    sketch out basic timeline loader function

--- js/app.js
+++ js/app.js
@@ -6,25 +6,16 @@ window.loadScript = function(scriptName) {
   scriptElement.src = 'js/' + scriptName + '.js';
 
   if (!window[scriptName]) {
-    // document.write('<script src="js/'+ scriptName +'.js"><\/script>');
     document.getElementsByTagName('head')[0].appendChild(scriptElement);
   }
 };
 
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//   if (xmlhttp.readyState == 4 ) {
-//     if (xmlhttp.status == 200){
-//       document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
-//     }
-//     else if (xmlhttp.status == 400) {
-//       console.error('There was an error 400');
-//     }
-//     else {
-//       console.warn('something else other than 200 was returned');
-//     }
-//   }
-// }
+// loadScript('force');
 
-// xmlhttp.open("GET", "config.json", true);
-// xmlhttp.send();
+function tngTimeline(el) {
+  el = el || 'body';
+  loadScript('timeline');
+  d3.json('data/tng.json', function(error, response) {
+    t = new Timeline(el, { data: response });
+  });
+}

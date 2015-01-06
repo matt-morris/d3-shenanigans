3890604e491bf6da8b23709e7e2c36a4399955ba
Matt Morris <matt.morris@placewise.com>
2015-01-05 17:40:30 -0700

    add options hash to function k

--- js/k.js
+++ js/k.js
@@ -1,11 +1,21 @@
-function k(n, selector) {
-  selector = selector || 'body';
-  var kn = new Graph(selector);
+function k(n, options) {
+  n = n || 0;
+  var selector;
+  options = options || {};
+  if (typeof options == 'string') {
+    selector = options;
+  }
+  else {
+    selector = options.selector || 'body';
+  }
+  graph = new Graph(selector, options);
 
   for (var i = 0; i < n; i++) {
-    kn.addNode({ id: i });
+    graph.addNode({ id: i });
     for (var j = 0; j < i; j++) {
-      kn.addLink({ source: i, target: j });
+      graph.addLink({ source: i, target: j });
     }
   }
+
+  return graph;
 }

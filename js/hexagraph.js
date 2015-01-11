5727fe23360501c20a32d652b699f3e787260fd3
Matt Morris <matt.morris@placewise.com>
2015-01-11 00:48:54 -0700

    clean up hexagraph logic

--- js/hexagraph.js
+++ js/hexagraph.js
@@ -1,14 +1,13 @@
 function hex() {
   var g = new Graph();
+
   for (var i = 0; i < 6; i++) {
     g.addNode({ charge: -75 });
   }
 
-  for (var j = 0; j < 5; j++) {
-    g.addLink({ source: j, target: j+1, length: 20 });
+  for (var j = 0; j < 6; j++) {
+    g.addLink({ source: j % 6, target: (j+1) % 6, length: 20 });
   }
-
-  g.addLink({ source: 5, target: 0, length: 20 });
 
   return g;
 }

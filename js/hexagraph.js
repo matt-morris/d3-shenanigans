61fc15846d5e71e6026ead9d7a20271c84d42dfb
Matt Morris <matt.morris@placewise.com>
2015-01-11 00:53:29 -0700

    optionally pass in an excisting graph

--- js/hexagraph.js
+++ js/hexagraph.js
@@ -1,12 +1,12 @@
-function hex() {
-  var g = new Graph();
+function hex(graph) {
+  graph = graph || new Graph();
 
   for (var i = 0; i < 6; i++) {
-    g.addNode({ charge: -75 });
+    graph.addNode({ charge: -75 });
   }
 
   for (var j = 0; j < 6; j++) {
-    g.addLink({ source: j % 6, target: (j+1) % 6, length: 20 });
+    graph.addLink({ source: j % 6, target: (j+1) % 6, length: 20 });
   }
 
   return g;

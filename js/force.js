50889f7029a7aca41646ae35b24b166bb4f0ee57
Matt Morris <matt.morris@placewise.com>
2015-02-05 21:01:09 -0700

    replace != with !==

--- js/force.js
+++ js/force.js
@@ -146,7 +146,7 @@ function Graph(selector, options) {
 
   vis.on('click', function() {
     // console.log(d3.event)
-    if (d3.event.altKey && d3.event.target.nodeName != 'circle') {
+    if (d3.event.altKey && d3.event.target.nodeName !== 'circle') {
       self.addNode({ x: d3.event.x, y: d3.event.y });
     }
   });
